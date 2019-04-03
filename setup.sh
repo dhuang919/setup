09a9cdbfea22d945548da9a83549d67a0476b063
Derek Huang <derekh919@gmail.com>
2019-04-03 15:33:12 -0400

    adjust set_host

--- setup.sh
+++ setup.sh
@@ -14,8 +14,6 @@ function set_host {
   echo "Setting host names"
   local HOSTNAME='derek'
   sudo scutil --set ComputerName $HOSTNAME
-  sudo scutil --set HostName $HOSTNAME
-  sudo scutil --set LocalHostName $HOSTNAME
   sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $HOSTNAME
 }
 
