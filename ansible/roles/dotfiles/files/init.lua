61f39573b070950ae6fe836f5caa832da1230489
Derek Huang <derekh919@gmail.com>
2020-10-04 21:35:08 -0400

    add init.lua

---
+++ ansible/roles/dotfiles/files/init.lua
@@ -0,0 +1,45 @@
+-- apps = {
+--   Color LCD = {  -- main (1)
+--     Slack = {
+--       size = 999.0, 776.0,
+--       topLeft = 407.0, 56.0,
+--     },
+--     Calendar = {
+--       size = 956.0, 598.0,
+--       topLeft = 436.0, 265.0,
+--     },
+--     Spotify = {
+--       size = 1023.0, 660.0,
+--       topLeft = 17.0, 216.0,
+--     },
+--     Mail = {
+--       size = 1113.0, 675.0,
+--       topLeft = 30.0, 46.0,
+--     },
+--   },
+
+--   DELL U2518D = {  -- horizontal (2)
+--     Typora = {
+--       size = 1042.0, 913.0,
+--       topLeft = 135.0, -1197.0,
+--     },
+--   },
+
+--   DELL U2515H = {  -- vertical (3)
+--     Google Chrome = {
+--       size = 1440.0, 921.0,
+--       topLeft = 1440.0, -1321.0,
+--     },
+--     iTerm2 = {
+--       size = 1440.0, 1098.0,
+--       topLeft = 1440.0, -399.0,
+--     },
+--   },
+-- }
+
+hs.hotkey.bind({'alt', 'cmd', 'ctrl'}, 'W', function()
+  local displays = hs.screen.allScreens()
+  local foo = hs.application.open('spotify', 5, true)
+  foo:getWindow().moveToScreen(displays[1], false, true)
+end)
+
