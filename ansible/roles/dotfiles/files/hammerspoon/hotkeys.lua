cb394818e28aa82ee04a5c67e8b0633d0cae24cc
Derek Huang <derekh919@gmail.com>
2020-10-08 13:19:37 -0400

    adjust chrome in hammerspoon

--- ansible/roles/dotfiles/files/hammerspoon/hotkeys.lua
+++ ansible/roles/dotfiles/files/hammerspoon/hotkeys.lua
@@ -8,7 +8,7 @@ hs.hotkey.bind({'alt', 'cmd', 'ctrl'}, 'W', function()
   o.openAndMove("Spotify", "LAPTOP", 0.233, 0.01, 0.77, 0.7)
   o.openAndMove("Calendar", "LAPTOP", 0.31, 0.287, 0.68, 0.67)
 
-  o.openAndMove("Google Chrome", "VERTICAL", 0.2088, 0, 0.359, 1)
+  o.openAndMove("Google Chrome", "VERTICAL", 0.19, 0, 0.378, 1)
   o.openAndMove("iTerm", "VERTICAL", 0.568, 0, 0.44, 1)
 
   o.openAndMove("Typora", "HORIZONTAL", 0.16, 0.428, 0.718, 0.462)
