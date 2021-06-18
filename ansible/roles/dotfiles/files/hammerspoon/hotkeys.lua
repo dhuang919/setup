634a4ae6ae0eb3c19bee0da202a45c3f10e0a647
Derek Huang <derekh919@gmail.com>
2021-06-18 10:38:23 -0400

    add outlook and insomnia to hammerspoon

--- ansible/roles/dotfiles/files/hammerspoon/hotkeys.lua
+++ ansible/roles/dotfiles/files/hammerspoon/hotkeys.lua
@@ -1,6 +1,41 @@
 local o = require('open')
 
 local ratios = {
+  calendar = {
+    full_screen = false,
+    top = 0.31,
+    left = 0.287,
+    height = 0.68,
+    width = 0.67,
+  },
+  chrome = {
+    full_screen = false,
+    top = 0.19,
+    left = 0,
+    height = 0.378,
+    width = 1,
+  },
+  insomnia = {
+    full_screen = false,
+    top = 0.308,
+    left = 0.312,
+    height = 0.58,
+    width = 0.56,
+  },
+  iterm = {
+    full_screen = false,
+    top = 0.568,
+    left = 0,
+    height = 0.44,
+    width = 1,
+  },
+  outlook = {
+    full_screen = false,
+    top = 0.1,
+    left = 0.132,
+    height = 0.65,
+    width = 0.57,
+  },
   slack = {
     full_screen = false,
     top = 0.063,
@@ -15,13 +50,6 @@ local ratios = {
     height = 0.77,
     width = 0.7,
   },
-  calendar = {
-    full_screen = false,
-    top = 0.31,
-    left = 0.287,
-    height = 0.68,
-    width = 0.67,
-  },
   typora = {
     full_screen = false,
     top = 0.16,
@@ -29,29 +57,17 @@ local ratios = {
     height = 0.55,
     width = 0.4,
   },
-  chrome = {
-    full_screen = false,
-    top = 0.19,
-    left = 0,
-    height = 0.378,
-    width = 1,
-  },
-  iterm = {
-    full_screen = false,
-    top = 0.568,
-    left = 0,
-    height = 0.44,
-    width = 1,
-  },
 }
 
 hs.hotkey.bind({'alt', 'cmd', 'ctrl'}, 'W', function()
+  -- o.moveIfOpen('Calendar', 'LAPTOP', ratios.calendar)
   o.moveIfOpen('Slack', 'LAPTOP', ratios.slack)
   o.moveIfOpen('Spotify', 'LAPTOP', ratios.spotify)
-  o.moveIfOpen('Calendar', 'LAPTOP', ratios.calendar)
 
+  o.moveIfOpen('Insomnia', 'HORIZONTAL', ratios.insomnia)
+  o.moveIfOpen('Microsoft Outlook', 'HORIZONTAL', ratios.outlook)
   o.moveIfOpen('Typora', 'HORIZONTAL', ratios.typora)
 
-  o.moveIfOpen('Google Chrome', 'VERTICAL', ratios.chrome)
   o.moveIfOpen('iTerm2', 'VERTICAL', ratios.iterm)
+  o.moveIfOpen('Google Chrome', 'VERTICAL', ratios.chrome)
 end)
